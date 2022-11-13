03a5d67aa7cbb4a698bf8d991abfbd1be8c2c8bb
Akash NP <akashnp1998@gmail.com>
2022-11-13 14:20:18 +0530

    Inital Commit, With TailwindCSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bnx-employee-portal-ng';
+  title: string = 'bnx-employee-portal-ng';
 }
